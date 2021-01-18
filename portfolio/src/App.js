b6e482ad83a88e9166f51ffb532748c6ae62f8ab
jhf1203 <jhf1203@gmail.com>
2021-01-18 12:24:11 -0500

    changed router

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { HashRouter as Router, Route } from "react-router-dom";
 
 import About from "./components/pages/About";
 import Portfolio from "./components/pages/Portfolio"
