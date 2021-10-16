58345fd8384d24c332a27a53306c2b66526ddd02
jhf1203 <jhf1203@gmail.com>
2021-10-15 21:46:41 -0400

    added key to backend

--- server/server.js
+++ server/server.js
@@ -1,10 +1,16 @@
 const express = require("express")
 const mongoose = require("mongoose")
+const routes = require("./routes")
 // add routes var
+
+require("dotenv").config()
 
 const app = express()
 const PORT = process.env.PORT || 3001
 
+app.use(routes)
+
 app.listen(PORT, () => {
+    console.log("env", process.env)
     console.log(`now listening on port# ${PORT}`)
 })
